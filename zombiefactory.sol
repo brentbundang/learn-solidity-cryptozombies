73184334c688999cd85096c6ab4cef7242212c71
breent <brent.bundang@gmail.com>
2021-07-04 13:22:57 -0400

    Added the level, and cooldown time variables for our zombie struct.

--- zombiefactory.sol
+++ zombiefactory.sol
@@ -1,14 +1,16 @@
 pragma solidity >=0.5.0 <0.6.0;
 
-import "./ownable.sol"
+import "./ownable.sol";
 
-
-contract ZombieFactory is Ownable{
+contract ZombieFactory is Ownable {
 
     event NewZombie(uint zombieId, string name, uint dna);
 
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
+    // 1. Define `cooldownTime` here
+    uint cooldownTime = 1 days;
+
 
     struct Zombie {
         string name;
@@ -23,7 +25,8 @@ contract ZombieFactory is Ownable{
     mapping (address => uint) ownerZombieCount;
 
     function _createZombie(string memory _name, uint _dna) internal {
-        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        // 2. Update the following line:
+        uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime))) - 1;
         zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
         emit NewZombie(id, _name, _dna);
